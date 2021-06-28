b12eb8a1f5391adc54a800dbd8391585d4a8265a
Luc <lucasribeirodesouza@live.com>
2021-06-28 12:11:04 -0300

    App

---
+++ src/services/SettingsService.ts
@@ -0,0 +1,52 @@
+import { getCustomRepository, Repository } from 'typeorm';
+import { Setting } from '../entities/Setting';
+import { SettingsRepository } from '../repositories/SettingsRepository';
+
+interface SettingsProps {
+  chat: boolean;
+  username: string;
+}
+
+class SettingsService {
+  private settingsRepository: Repository<Setting>;
+
+  constructor() {
+    this.settingsRepository = getCustomRepository(SettingsRepository);
+  }
+
+  async create({ chat, username }: SettingsProps) {
+    // Select * from settings where username = "username" limit 1;
+    const userAlreadyExists = await this.settingsRepository.findOne({
+      username,
+    });
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const settings = this.settingsRepository.create({
+      chat,
+      username,
+    });
+
+    await this.settingsRepository.save(settings);
+
+    return settings;
+  }
+
+  async findByUserName(username: string) {
+    const settings = await this.settingsRepository.findOne({ username });
+    return settings;
+  }
+
+  async update(username: string, chat: boolean) {
+    await this.settingsRepository
+      .createQueryBuilder()
+      .update(Setting)
+      .set({ chat })
+      .where('username = :username', { username })
+      .execute();
+  }
+}
+
+export { SettingsService };
